b969f5dcb6ed4fe36c0a6125f56937d9e7dcfbbc
Ray Lee <cheuk3@gmail.com>
2024-12-09 22:06:37 +0800

    refactor: streamline SQL query in UpdateTender mutation
    - Removed an unnecessary trailing comma in the SQL column definitions of the UpdateTender mutation.
    - This change improves code readability and ensures proper SQL syntax, preventing potential execution errors during tender updates.

--- graphql/mutation.resolvers.go
+++ graphql/mutation.resolvers.go
@@ -217,7 +217,7 @@ func (r *mutationResolver) UpdateTender(ctx context.Context, id xid.ID, input en
 					"ZXMDY",
 					"ZXMLX"
 				) VALUES (
-					?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?
+					?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?
 				)
 			`, mandt, t.Code, t.Name, t.Edges.Area.Name, customerName, t.Edges.Finder.Name, t.DiscoveryDate, t.Edges.CreatedBy.Name, t.CreatedAt, t.Status, followingSalesNamesStr, t.FullAddress, t.EstimatedAmount, t.TenderDate, t.ProjectCode, t.ProjectDefinition, t.ProjectType)
 			if err != nil {
