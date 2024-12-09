f582da70efaef7e5c9e94e965b568856f4626794
Ray Lee <cheuk3@gmail.com>
2024-12-09 21:47:38 +0800

    feat: add create and update customer mutations with GraphQL integration
    - Introduced `useCreateCustomerMutation` and `useUpdateCustomerMutation` to facilitate customer creation and updates in the application.
    - The `useCreateCustomerMutation` includes input fields for customer details such as name, contact person, and industry, enhancing data entry capabilities.
    - The `useUpdateCustomerMutation` allows for flexible updates to customer information, including options to clear or modify existing fields.
    - Both mutations utilize the `customerDetailFragment` for consistent data retrieval, improving integration with existing customer-related components.
    - These enhancements streamline customer management processes and improve overall user experience.

---
+++ web/__generated__/useCreateCustomerMutation.graphql.ts
@@ -0,0 +1,302 @@
+/**
+ * @generated SignedSource<<4099efcaa787f8394e1e9ee2080eebd7>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
+export type CreateCustomerInput = {
+  areaID: string;
+  contactPerson?: string | null | undefined;
+  contactPersonEmail?: string | null | undefined;
+  contactPersonPhone?: string | null | undefined;
+  contactPersonPosition?: string | null | undefined;
+  createdAt?: any | null | undefined;
+  createdByID: string;
+  industry: number;
+  name: string;
+  ownerType?: number | null | undefined;
+  salesID?: string | null | undefined;
+  size?: number | null | undefined;
+  tenderIDs?: ReadonlyArray<string> | null | undefined;
+  updatedAt?: any | null | undefined;
+  visitRecordIDs?: ReadonlyArray<string> | null | undefined;
+};
+export type useCreateCustomerMutation$variables = {
+  connections: ReadonlyArray<string>;
+  input: CreateCustomerInput;
+};
+export type useCreateCustomerMutation$data = {
+  readonly createCustomer: {
+    readonly edges: ReadonlyArray<{
+      readonly node: {
+        readonly " $fragmentSpreads": FragmentRefs<"customerDetailFragment">;
+      } | null | undefined;
+    } | null | undefined> | null | undefined;
+  };
+};
+export type useCreateCustomerMutation = {
+  response: useCreateCustomerMutation$data;
+  variables: useCreateCustomerMutation$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = {
+  "defaultValue": null,
+  "kind": "LocalArgument",
+  "name": "connections"
+},
+v1 = {
+  "defaultValue": null,
+  "kind": "LocalArgument",
+  "name": "input"
+},
+v2 = [
+  {
+    "kind": "Variable",
+    "name": "input",
+    "variableName": "input"
+  }
+],
+v3 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v4 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "name",
+  "storageKey": null
+},
+v5 = [
+  (v4/*: any*/),
+  (v3/*: any*/)
+];
+return {
+  "fragment": {
+    "argumentDefinitions": [
+      (v0/*: any*/),
+      (v1/*: any*/)
+    ],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "useCreateCustomerMutation",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v2/*: any*/),
+        "concreteType": "CustomerConnection",
+        "kind": "LinkedField",
+        "name": "createCustomer",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "CustomerEdge",
+            "kind": "LinkedField",
+            "name": "edges",
+            "plural": true,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "Customer",
+                "kind": "LinkedField",
+                "name": "node",
+                "plural": false,
+                "selections": [
+                  {
+                    "args": null,
+                    "kind": "FragmentSpread",
+                    "name": "customerDetailFragment"
+                  }
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Mutation",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [
+      (v1/*: any*/),
+      (v0/*: any*/)
+    ],
+    "kind": "Operation",
+    "name": "useCreateCustomerMutation",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v2/*: any*/),
+        "concreteType": "CustomerConnection",
+        "kind": "LinkedField",
+        "name": "createCustomer",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "CustomerEdge",
+            "kind": "LinkedField",
+            "name": "edges",
+            "plural": true,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "Customer",
+                "kind": "LinkedField",
+                "name": "node",
+                "plural": false,
+                "selections": [
+                  (v3/*: any*/),
+                  (v4/*: any*/),
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "User",
+                    "kind": "LinkedField",
+                    "name": "createdBy",
+                    "plural": false,
+                    "selections": (v5/*: any*/),
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "updatedAt",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "ownerType",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "industry",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "size",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "contactPerson",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "contactPersonPosition",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "contactPersonPhone",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "contactPersonEmail",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "User",
+                    "kind": "LinkedField",
+                    "name": "sales",
+                    "plural": false,
+                    "selections": (v5/*: any*/),
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "Area",
+                    "kind": "LinkedField",
+                    "name": "area",
+                    "plural": false,
+                    "selections": (v5/*: any*/),
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "filters": null,
+            "handle": "prependNode",
+            "key": "",
+            "kind": "LinkedHandle",
+            "name": "edges",
+            "handleArgs": [
+              {
+                "kind": "Variable",
+                "name": "connections",
+                "variableName": "connections"
+              },
+              {
+                "kind": "Literal",
+                "name": "edgeTypeName",
+                "value": "CustomerEdge"
+              }
+            ]
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "dd157b26fd3342bfaf64b7e3360ae2ca",
+    "id": null,
+    "metadata": {},
+    "name": "useCreateCustomerMutation",
+    "operationKind": "mutation",
+    "text": "mutation useCreateCustomerMutation(\n  $input: CreateCustomerInput!\n) {\n  createCustomer(input: $input) {\n    edges {\n      node {\n        ...customerDetailFragment\n        id\n      }\n    }\n  }\n}\n\nfragment customerDetailFragment on Customer {\n  id\n  name\n  createdBy {\n    name\n    id\n  }\n  updatedAt\n  ownerType\n  industry\n  size\n  contactPerson\n  contactPersonPosition\n  contactPersonPhone\n  contactPersonEmail\n  sales {\n    name\n    id\n  }\n  area {\n    name\n    id\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "e236c02cdb9adec4e61df4c77db7e13e";
+
+export default node;
---
+++ web/__generated__/useUpdateCustomerMutation.graphql.ts
@@ -0,0 +1,245 @@
+/**
+ * @generated SignedSource<<dd536820fb34b4d10b34b3816649b262>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
+export type UpdateCustomerInput = {
+  addTenderIDs?: ReadonlyArray<string> | null | undefined;
+  addVisitRecordIDs?: ReadonlyArray<string> | null | undefined;
+  areaID?: string | null | undefined;
+  clearContactPerson?: boolean | null | undefined;
+  clearContactPersonEmail?: boolean | null | undefined;
+  clearContactPersonPhone?: boolean | null | undefined;
+  clearContactPersonPosition?: boolean | null | undefined;
+  clearIndustry?: boolean | null | undefined;
+  clearOwnerType?: boolean | null | undefined;
+  clearSales?: boolean | null | undefined;
+  clearSize?: boolean | null | undefined;
+  clearTenders?: boolean | null | undefined;
+  clearVisitRecords?: boolean | null | undefined;
+  contactPerson?: string | null | undefined;
+  contactPersonEmail?: string | null | undefined;
+  contactPersonPhone?: string | null | undefined;
+  contactPersonPosition?: string | null | undefined;
+  createdByID?: string | null | undefined;
+  industry?: number | null | undefined;
+  name?: string | null | undefined;
+  ownerType?: number | null | undefined;
+  removeTenderIDs?: ReadonlyArray<string> | null | undefined;
+  removeVisitRecordIDs?: ReadonlyArray<string> | null | undefined;
+  salesID?: string | null | undefined;
+  size?: number | null | undefined;
+  updatedAt?: any | null | undefined;
+};
+export type useUpdateCustomerMutation$variables = {
+  id: string;
+  input: UpdateCustomerInput;
+};
+export type useUpdateCustomerMutation$data = {
+  readonly updateCustomer: {
+    readonly " $fragmentSpreads": FragmentRefs<"customerDetailFragment">;
+  };
+};
+export type useUpdateCustomerMutation = {
+  response: useUpdateCustomerMutation$data;
+  variables: useUpdateCustomerMutation$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "id"
+  },
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "input"
+  }
+],
+v1 = [
+  {
+    "kind": "Variable",
+    "name": "id",
+    "variableName": "id"
+  },
+  {
+    "kind": "Variable",
+    "name": "input",
+    "variableName": "input"
+  }
+],
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v3 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "name",
+  "storageKey": null
+},
+v4 = [
+  (v3/*: any*/),
+  (v2/*: any*/)
+];
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "useUpdateCustomerMutation",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": "Customer",
+        "kind": "LinkedField",
+        "name": "updateCustomer",
+        "plural": false,
+        "selections": [
+          {
+            "args": null,
+            "kind": "FragmentSpread",
+            "name": "customerDetailFragment"
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Mutation",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "useUpdateCustomerMutation",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": "Customer",
+        "kind": "LinkedField",
+        "name": "updateCustomer",
+        "plural": false,
+        "selections": [
+          (v2/*: any*/),
+          (v3/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "User",
+            "kind": "LinkedField",
+            "name": "createdBy",
+            "plural": false,
+            "selections": (v4/*: any*/),
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "updatedAt",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "ownerType",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "industry",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "size",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "contactPerson",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "contactPersonPosition",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "contactPersonPhone",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "contactPersonEmail",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "User",
+            "kind": "LinkedField",
+            "name": "sales",
+            "plural": false,
+            "selections": (v4/*: any*/),
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "Area",
+            "kind": "LinkedField",
+            "name": "area",
+            "plural": false,
+            "selections": (v4/*: any*/),
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "0ddc49c243ceb0055efa893e8b478435",
+    "id": null,
+    "metadata": {},
+    "name": "useUpdateCustomerMutation",
+    "operationKind": "mutation",
+    "text": "mutation useUpdateCustomerMutation(\n  $id: ID!\n  $input: UpdateCustomerInput!\n) {\n  updateCustomer(id: $id, input: $input) {\n    ...customerDetailFragment\n    id\n  }\n}\n\nfragment customerDetailFragment on Customer {\n  id\n  name\n  createdBy {\n    name\n    id\n  }\n  updatedAt\n  ownerType\n  industry\n  size\n  contactPerson\n  contactPersonPosition\n  contactPersonPhone\n  contactPersonEmail\n  sales {\n    name\n    id\n  }\n  area {\n    name\n    id\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "409cb914a3eb31be394ed2c41a8583c5";
+
+export default node;
