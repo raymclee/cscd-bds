42bb2a367f0f9dd8f607fb5e47b456cef4cba670
Ray Lee <cheuk3@gmail.com>
2025-03-19 12:27:45 +0800

    feat: 新增 v2PageDistrictQuery 和 v2PageQuery 的 GraphQL 类型定义
    - 添加 v2PageDistrictQuery，用于获取地区及其相关的地块信息
    - 添加 v2PageQuery，支持用户、客户和招标信息的查询
    - 更新 GraphQL 架构以支持新的查询功能，增强数据获取能力

---
+++ web/__generated__/v2PageDistrictQuery.graphql.ts
@@ -0,0 +1,222 @@
+/**
+ * @generated SignedSource<<350f94cc3e1d080cfe7af2f924b05f2a>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest } from 'relay-runtime';
+export type v2PageDistrictQuery$variables = {
+  adcode: number;
+};
+export type v2PageDistrictQuery$data = {
+  readonly districts: {
+    readonly edges: ReadonlyArray<{
+      readonly node: {
+        readonly plots: {
+          readonly edges: ReadonlyArray<{
+            readonly node: {
+              readonly colorHex: string;
+              readonly geoBounds: ReadonlyArray<ReadonlyArray<number>> | null | undefined;
+              readonly id: string;
+              readonly name: string;
+            } | null | undefined;
+          } | null | undefined> | null | undefined;
+        };
+      } | null | undefined;
+    } | null | undefined> | null | undefined;
+  };
+};
+export type v2PageDistrictQuery = {
+  response: v2PageDistrictQuery$data;
+  variables: v2PageDistrictQuery$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "adcode"
+  }
+],
+v1 = [
+  {
+    "fields": [
+      {
+        "kind": "Variable",
+        "name": "adcode",
+        "variableName": "adcode"
+      }
+    ],
+    "kind": "ObjectValue",
+    "name": "where"
+  }
+],
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v3 = {
+  "alias": null,
+  "args": null,
+  "concreteType": "PlotConnection",
+  "kind": "LinkedField",
+  "name": "plots",
+  "plural": false,
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": "PlotEdge",
+      "kind": "LinkedField",
+      "name": "edges",
+      "plural": true,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "Plot",
+          "kind": "LinkedField",
+          "name": "node",
+          "plural": false,
+          "selections": [
+            (v2/*: any*/),
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "name",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "geoBounds",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "colorHex",
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        }
+      ],
+      "storageKey": null
+    }
+  ],
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "v2PageDistrictQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": "DistrictConnection",
+        "kind": "LinkedField",
+        "name": "districts",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "DistrictEdge",
+            "kind": "LinkedField",
+            "name": "edges",
+            "plural": true,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "District",
+                "kind": "LinkedField",
+                "name": "node",
+                "plural": false,
+                "selections": [
+                  (v3/*: any*/)
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "v2PageDistrictQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": "DistrictConnection",
+        "kind": "LinkedField",
+        "name": "districts",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "DistrictEdge",
+            "kind": "LinkedField",
+            "name": "edges",
+            "plural": true,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "District",
+                "kind": "LinkedField",
+                "name": "node",
+                "plural": false,
+                "selections": [
+                  (v3/*: any*/),
+                  (v2/*: any*/)
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "d376a60bae454dfb3a8c93b58424e175",
+    "id": null,
+    "metadata": {},
+    "name": "v2PageDistrictQuery",
+    "operationKind": "query",
+    "text": "query v2PageDistrictQuery(\n  $adcode: Int!\n) {\n  districts(where: {adcode: $adcode}) {\n    edges {\n      node {\n        plots {\n          edges {\n            node {\n              id\n              name\n              geoBounds\n              colorHex\n            }\n          }\n        }\n        id\n      }\n    }\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "40a3d4e87f3dd3b502e4d7dc2b2a0816";
+
+export default node;
